e11127ae9c5e8d4c98ec7f15ca44967b878e443e
Vection <avivzx@gmail.com>
2018-05-09 11:54:16 +0300

    Add files via upload

--- Board.h
+++ Board.h
@@ -20,20 +20,22 @@ public:
 	Board::Board() {
 		mat = new char(5);
 		this->length = 5;
+		for (int i = 0; i < length*length; i++) { mat[i] = '.'; }
 	}
 	Board::Board(int num) {
 		this->length = num;
-		mat = new char(num);
+		mat = new char(num*num);
+		for (int i = 0; i < length*length; i++) { mat[i] = '.'; }
 	}
 
 	char& operator [](initializer_list<int> p)
 	{
-		
-		int a = returnsyntax(*p.begin(), *p.end());
+		int a = returnsyntax(*p.begin(), *p.begin() + 1);
 		return mat[a];
 	}
-	int returnsyntax(const int x, const int y) {
-		return length*x + y;
+	int returnsyntax(const int x, const int y) {	
+		return this->length*x + y;
+		
 	}
 	friend ostream &operator<<(ostream &output, const Board &temp) {
 		string p = "";
@@ -41,7 +43,7 @@ public:
 		for (int i = 0; i < temp.length*temp.length; i++) {
 			p += temp.mat[i];
 			counter++;
-			if(counter == temp.length-1) {
+			if(counter == temp.length) {
 				p += "\n";
 				counter = 0;
 			}
